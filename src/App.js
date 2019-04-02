fc0453065b2bd923d5da25f05e21f7b12f9dacb7
Urth <dragondor@protonmail.com>
2019-04-02 19:31:41 +0200

    5 voyages ajoutés

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import Travel from "./Travel";
-import Travels from "./Travels"
+import Travels from "./Travels";
+
 
 class App extends Component {
   render() {
@@ -13,7 +14,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Where I'd like to go.</h1>
         </header>
-        <Travel
+        {/* <Travel
           destination="Alpilles Mountains"
           country="France"
           photo="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Van_Gogh_The_Olive_Trees..jpg/240px-Van_Gogh_The_Olive_Trees..jpg"
@@ -26,7 +27,7 @@ class App extends Component {
           photo="https://cbsnews3.cbsistatic.com/hub/i/r/2014/03/01/81258710-7db3-4854-9a3c-1059e6f2d5ed/resize/620x465/4ef5b0ec54ff9d1a27d1fb13f71c4ca7/monument-valley-the-searchers-04.jpg#"
           credit="John Ford, 'The Searchers', 1956"
           distance=" 8500 km from Marseille"
-        />
+        /> */}
         <Travels/>
       </div>
     );
--- src/Travels.js
+++ src/Travels.js
@@ -1,47 +1,50 @@
 import React from "react";
+import Travel from "./Travel";
 const travels = [
     {
-        destination:"",
-        country:"",
-        photo:"",
-        credit:"",
-        distance:"",
+        destination:"Kyoto",
+        country:"Japan",
+        photo:"https://upload.wikimedia.org/wikipedia/commons/4/42/Hiroshige_nuit_de_neige_%C3%A0_Kambara.JPG",
+        credit:"Hirochige, 'Nuit de neige à Kambara', estampe, XIX° siècle ",
+        distance:"9630 km from Paris.",
     },
     {
-        destination:"",
-        country:"",
-        photo:"",
+        destination:"musée d'Árbær",
+        country:"Islande",
+        photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Islande_-_Ferme.JPG/800px-Islande_-_Ferme.JPG",
         credit:"",
-        distance:"",
+        distance:"3500 km from Marseille.",
     },
     {
-        destination:"",
-        country:"",
-        photo:"",
-        credit:"",
-        distance:"",
+        destination:"Vosges",
+        country:"France",
+        photo:"http://gerardmerinfo.fr/wp-content/uploads/2016/01/Col_Vosges_Neige_01.jpg",
+        credit:"via http://gerardmer.info.fr",
+        distance:"689 km from Marseille",
     },
     {
-        destination:"",
-        country:"",
-        photo:"",
-        credit:"",
-        distance:"",
+        destination:"Scotland",
+        country:"United Kingdom",
+        photo:"http://medieval.mrugala.net/Architecture/Ecosse,_Dornie,_Eilean_Donan,_Chateau/Ecosse,%20chateau%20Eilean%20Donan%20(1).jpg",
+        credit:"Chateau d'Elean Donan via 'http://medieval.mrugala.net/'",
+        distance:"1.887 km from Marseille.",
     },
     {
-        destination:"",
-        country:"",
-        photo:"",
-        credit:"",
-        distance:"",
+        destination:"Coimbra",
+        country:"Portugal",
+        photo:"https://nelsoncarvalheiro.com//wp-content/uploads/2013/11/Nelson_Carvalheiro_-1.jpg",
+        credit:"via http://nelsoncarvalheiro.com",
+        distance:"1.493 km from Marseille",
     }
 ];
 
-const Travels =() => (
+const Travels = () => (
     <div>
-        {travels.map(travel => (
-           <Travels destination = {travel.destination} country = {travel.country} photo = {travel.photo} credit = {travel.credit} distance = {travel.distance} />
+        {travels.map(travel => 
+        (
+           <Travel destination = {travel.destination} country = {travel.country} photo = {travel.photo} credit = {travel.credit} distance = {travel.distance} />
         )
+            // <Travels {... travel} />
             )}
     </div>
 );
